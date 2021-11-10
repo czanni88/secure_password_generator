10a3b960ec46f83bf1301cf13e4a054fc5395358
Cristiano <cristianopzanni@gmail.com>
2021-11-10 14:10:33 +0100

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+//Standalone runtime for Regenerator-compiled generator and async functions.
+require("regenerator-runtime/runtime");
+// The following line makes sure your styles are included in the project. Don't remove this.
+import '../styles/main.scss';
+// Import any additional modules you want to include below \/
+
+
+// \/ All of your javascript should go here \/
